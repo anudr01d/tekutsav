440f61c5f90086e5b4615a55061b9f295ad822ce
Anudeep Krishnadas <ankrishn@teksytems.com>
2018-02-08 19:38:51 +0530

    Got the app working properly on android

--- TEKUtsav.Droid/SplashActivity.cs
+++ TEKUtsav.Droid/SplashActivity.cs
@@ -4,13 +4,12 @@ using TEKUtsav.Droid;
 
 namespace TEKUtsav.Droid
 {
-    [Activity(Theme = "@style/SplashTheme",MainLauncher = true, NoHistory = true)]
+    [Activity(Theme = "@style/SplashTheme", NoHistory = true)]
 	public class SplashActivity : Activity
 	{
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
-			System.Threading.Thread.Sleep(2000);
 			this.StartActivity(typeof(MainActivity));
 		}
 	}
