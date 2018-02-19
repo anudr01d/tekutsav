5a331fbb9d5a3806b119bd738ae16289ac89d54a
Anil Manoharrao <abhimava@teksystems.com>
2018-02-19 19:32:56 +0530

    switch voting lines

--- TEKUtsav.Business/TEKUtsav.Ral.Impl/EventRestApi/EventRestApi.cs
+++ TEKUtsav.Business/TEKUtsav.Ral.Impl/EventRestApi/EventRestApi.cs
@@ -47,12 +47,12 @@ namespace TEKUtsav.Ral.Impl.EventRestApi
 
         public async Task<EventVotingSchedule> enableDiableVoting(EventVotingSchedule eventVoting)
         {
-            var azureclient = _azureClient.GetClient(Globals.EVENT_VOTING_SCHEDULE, string.Empty, string.Empty);
-            var table = _cloudService.GetTable<EventVotingSchedule>(azureclient);
-            var list = await table.CreateItemAsync(eventVoting);
-            return list;
-
-
+            var azureclient = _azureClient.GetClient(string.Empty, string.Empty, string.Empty);
+           // var table = _cloudService.GetTable<EventVotingSchedule>(azureclient);
+            //var list = await table.CreateItemAsync(eventVoting);
+            //return list;
+            await _cloudService.InvokeApiAsyncPostData<EventVotingSchedule,EventVotingSchedule>(azureclient, Globals.EVENT_VOTING_SCHEDULE, eventVoting);
+            return null;
         }
 
         public Task<List<EventWinner>> ComputeEventWinner(string eventTypeId)
